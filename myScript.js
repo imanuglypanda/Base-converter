2cc497b21b4412496f366b2fb7b0d33b9832ff71
Tanapatjeensamran <imanuglypanda@gmail.com>
2022-04-23 23:08:27 +0700

    first commit

---
+++ myScript.js
@@ -0,0 +1,51 @@
+function convertBase() {
+    let number = "";
+    number = document.getElementById("number").value;
+    let fromBase = Number(document.getElementById("fromBase").value);
+    try {
+        if (isInGivenBase(number, fromBase)) {
+            document.getElementById("number").style.backgroundColor = "white";
+            document.getElementById("result").innerHTML = parseInt(number, fromBase) /// Convert any base to base 10
+        } else {
+            document.getElementById("number").style.backgroundColor = "red";
+            document.getElementById("result").innerHTML = "Invalid input"
+        }
+    } catch(err) {
+        document.getElementById("number").style.backgroundColor = "red";
+        document.getElementById("result").innerHTML = err.message;
+    }
+}
+
+
+function isInGivenBase(str, base)
+{
+    try {
+        if (str === "") return false;
+        else if (isNaN(str)) return false;
+        else 
+        {
+            if (base > 16) return false;
+            else if (base <= 10) {
+                for (let i = 0; i < str.length; i++) {
+                    if (!(str[i].charCodeAt() >= '0'.charCodeAt() && 
+                    str[i].charCodeAt() < ('0'.charCodeAt() + base)))
+                        return false;
+                    }
+            } else {
+                for (let i = 0; i < str.length; i++) {
+                    if (! ((str[i].charCodeAt() >= '0'.charCodeAt() && 
+                    str[i].charCodeAt() < ('0'.charCodeAt() + base)) || 
+                    (str[i].charCodeAt() >= 'A'.charCodeAt() && str[i].charCodeAt() < 
+                    ('A'.charCodeAt() + base - 10))))		
+                        return false;
+                }
+            }
+            return true;
+        }
+    } catch(err) {
+        document.getElementById("number").style.backgroundColor = "red";
+        document.getElementById("result").innerHTML = err.message;
+    }
+}
+
+
